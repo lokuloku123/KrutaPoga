f9a28da80ce35dac0ea6accf5cf96e77b7dd5234
lokuloku123 <128679159+lokuloku123@users.noreply.github.com>
2024-04-23 08:38:47 +0300

    Added everything!

---
+++ script.js
@@ -0,0 +1,25 @@
+const poga = document.querySelector('.js-poga');
+
+let punkti = 10
+
+
+let randomColor = Math.floor(Math.random()*16777215).toString(16);
+
+poga.addEventListener('click', () => {
+    punkti = punkti - 1
+    console.log(punkti)
+    poga.style.backgroundColor = "#" + randomColor;
+
+    if(punkti === 0){
+        window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
+        punkti = punkti + 10;
+        poga.innerHTML = `Nospied mani vēl ${punkti}x`;
+
+    }else{
+        poga.innerHTML = `Nospied mani vēl ${punkti}x`;
+
+}} )
+
+let randomColor = Math.floor(Math.random()*16777215).toString(16);
+
+
